b783425a0c22acd9746f658b22b3f23ae5c272c0
Aya-Mohamed-Ashraf <aya.mohamed.ashraf@gmail.com>
2019-02-17 15:13:10 +0200

    Updates

---
+++ precache-manifest.a42264cf2298058e25daf41e64858c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d896615fd93aecb056ba",
+    "url": "/burger-shop/static/js/runtime~main.d896615f.js"
+  },
+  {
+    "revision": "22f9475c9cdbed77a010",
+    "url": "/burger-shop/static/js/main.22f9475c.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/burger-shop/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "22f9475c9cdbed77a010",
+    "url": "/burger-shop/static/css/main.ce1912d4.chunk.css"
+  },
+  {
+    "revision": "2499154606dee8bd7cccbeb26f7c99b3",
+    "url": "/burger-shop/index.html"
+  }
+];
